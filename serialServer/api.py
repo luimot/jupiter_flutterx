be914b00299c8a3dd4e484d3ac705a732e3c9a4f
Luiz Mota <luizmota@alunos.utfpr.edu.br>
2023-08-30 13:22:33 -0300

    Using websocket in API

--- serialServer/api.py
+++ serialServer/api.py
@@ -2,7 +2,25 @@ import sys
 import glob
 import json
 from typing import Union, List
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, WebSocket
+from starlette.responses import StreamingResponse
+import asyncio
+import serial
+
+# List for storing websocket client instances
+websocket_clients = []
+
+class connection:
+    ser = None
+    connected = None
+    
+    def __init__(self,port: str):
+        self.ser = serial.Serial(port)
+        self.connected = True
+    
+    def readPort(self,n_bytes: int):
+        if self.connected:
+            return ser.read(n_bytes)
 
 app = FastAPI()
 
@@ -23,10 +41,67 @@ def get_ports():
     availablePorts = listPorts()
     return {"availablePorts": availablePorts}
 
-@app.post("ports/{port}")
+@app.post("/ports/{port}")
 def selectPort(port: str):
-    # Checks again wheter the port is still usable
-    if port not in availablePorts():
+    # Checks again whether the port is still usable
+    print(port)
+    ports = listPorts()
+    for p in ports:
+        if port in p:
+            thisPort = p
+            hasMatch = True
+    if not hasMatch:
         raise HTTPException(status_code=404, detail="Serial port not found.")
-    # TODO: Implement serial logic
-    return{"confirmation": f"Port {port} selected"}
+    # TODO: Finish serial logic
+    conn = connection(thisPort)
+    return{"confirmation": f"Port {thisPort} selected"}
+
+# async def stream():
+#     async def event_generator():
+#         if conn.connected:
+#             while True:
+#                 data = conn.readPort(256)
+#                 if not data:
+#                     await asyncio.sleep(0.1)  # Aguarda 0.1 segundo se não houver dados
+#                     continue
+#                 yield b"data: " + data + b"\n\n"
+
+#     return StreamingResponse(event_generator(), media_type="text/event-stream")
+# ws
+# @app.get("/stream_ports")
+# async def get_stream_ports():
+#     return await stream()
+
+@app.websocket("/ws/{port}")
+async def websocket_endpoint(websocket: WebSocket,port: str):
+    ports = listPorts()
+    for p in ports:
+        if port in p:
+            thisPort = p
+            hasMatch = True
+    if not hasMatch:
+        raise HTTPException(status_code=404, detail="Serial port not found.")
+    # TODO: Finish serial logic
+    conn = connection(thisPort)
+    print("Listening to " + thisPort)
+    await websocket.accept()
+    websocket_clients.append(websocket)
+    if conn.connected:
+        print("Connected")
+        try:
+            while True:
+                print("Reading data...")
+                data = conn.ser.readline().decode().strip()  # Read from serial port
+                if data:
+                    print("Data acquired")
+                    await asyncio.gather(
+                        *[
+                            client.send_text(data) for client in websocket_clients
+                        ]
+                    )
+        except:
+            print("Exception while reading !")
+            pass
+        finally:
+            print("Disconnecting...")
+            websocket_clients.remove(websocket)
--- serialServer/connection.py
+++
@@ -1,7 +0,0 @@
-import serial
-
-class Connection{
-    ser = None
-    def initPort(port: str):
-        ser = serial.Serial(port)
-}
